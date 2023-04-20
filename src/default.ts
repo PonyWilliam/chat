f499827d59b1429c152a8458d412954f08892dcf
WilliamTan <tjw@dadiqq.cn>
2023-04-21 01:38:45 +0800

    first

---
+++ src/default.ts
@@ -0,0 +1,11 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 本站服务器搭建在美国，可直接访问到openai，响应速度较快。
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
